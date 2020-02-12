635d932b30ff36238d97d41e33b39fff84764829
jdaMachado <34648293+jdaMachado@users.noreply.github.com>
2020-02-12 18:33:40 +0000

    Add files via upload
    Initial upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+resource "aws_route53_zone" "domain" { 
+    name  = var.domain_name
+}
+
+resource "aws_route53_record" "naked_host" {
+    count   = var.create_naked ? 1 : 0 
+    zone_id = aws_route53_zone.domain.id
+    name    = var.domain_name
+    type    = "A"
+    ttl     = var.ttl
+    records = var.ipv6 != "" ? [var.ipv4, var.ipv6] : [var.ipv4]
+}
+
+resource "aws_route53_record" "hosts" {
+    count   = length(var.hosts)
+    zone_id = aws_route53_zone.domain.id
+    name    = var.hosts[count.index]["name"]
+    type    = var.hosts[count.index]["type"] 
+    ttl     = var.hosts[count.index]["ttl"]
+    records = var.hosts[count.index]["records"]
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "domain_name" {
+    type = string
+    default = ""
+}
+
+variable "ttl" {
+    type = string
+    default = "3600"
+}
+
+variable "ipv4" {
+    type = string
+    default = ""
+}
+
+variable "ipv6" {
+    type = string
+    default = ""
+}
+
+variable "hosts" {
+    type = list(map(any))
+    default = []
+}
+
+variable "create_naked" {
+    type = bool
+    default = false
+}
